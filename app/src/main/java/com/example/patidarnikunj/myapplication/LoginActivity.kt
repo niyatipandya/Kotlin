819637ca269cec9a811a45a367f016c9096da772
niyatipandya <niyatipandya302@gmail.com>
2017-11-24 12:51:03 +0530

    Third Commit

--- app/src/main/java/com/example/patidarnikunj/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/patidarnikunj/myapplication/LoginActivity.kt
@@ -85,7 +85,6 @@ public class LoginActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
         FacebookSdk.sdkInitialize(this.applicationContext)
         callbackManager = CallbackManager.Factory.create()
